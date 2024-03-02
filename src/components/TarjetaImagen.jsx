48d479357f79a658c995efddeed39902e433cfc0
Mildred <a1123250021@utch.edu.mx>
2024-03-01 19:59:14 -0700

    Avance api

--- src/components/TarjetaImagen.jsx
+++ src/components/TarjetaImagen.jsx
@@ -1,14 +1,40 @@
 // TarjetaImagen.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TarjetaImagen = ({ mensaje, eliminar, editar }) => {
   const [editando, setEditando] = useState(false);
   const [nuevoMensaje, setNuevoMensaje] = useState(mensaje);
+  const [codigoQR, setCodigoQR] = useState('');
 
   const handleGuardar = () => {
     editar(nuevoMensaje);
     setEditando(false);
   };
+
+  useEffect(() => {
+    const generarCodigoQR = async () => {
+      const datosParaQR = nuevoMensaje || mensaje;
+
+      try {
+        // Aqui utilizo la API de Google Chart QR Code para generar el código QR pero me manda a buscar lo que introduci en google
+        const response = await fetch(
+          `https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=${encodeURIComponent(
+            datosParaQR
+          )}`
+        );
+
+        if (response.ok) {
+          setCodigoQR(response.url);
+        } else {
+          console.error('Error al generar el código QR');
+        }
+      } catch (error) {
+        console.error('Error de red al generar el código QR', error);
+      }
+    };
+
+    generarCodigoQR();
+  }, [mensaje, nuevoMensaje]);
 
   return (
     <div className="tarjeta-imagen">
@@ -24,6 +50,7 @@ const TarjetaImagen = ({ mensaje, eliminar, editar }) => {
       ) : (
         <>
           <p>{mensaje}</p>
+          {codigoQR && <img src={codigoQR} alt="Código QR" />}
           <button onClick={() => setEditando(true)}>Editar</button>
           <button onClick={eliminar}>Eliminar</button>
         </>
